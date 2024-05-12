69ad7a35c204ac0a357dfa5f0ae3155fa3b2f45b
Colten Begle <coltenbegle@icloud.com>
2024-05-11 23:04:31 -0400

    refactoring logic

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define ROWS 25
-#define COLS 50
+#define ROWS 10
+#define COLS 20
 
 #define clear() printf("\033[H\033[J")
 
@@ -24,9 +24,9 @@ char **generate_seed(int rows, int cols) {
 
   for (int row = 0; row < ROWS; row++) {
     for (int col = 0; col < COLS; col++) {
-      int isAlive = rand() % (1 + 1);
+      int isAlive = rand() % (10 + 1);
 
-      if (isAlive == 1) {
+      if (isAlive <= 3) {
         seed[row][col] = 'O';
       } else {
         seed[row][col] = ' ';
@@ -37,47 +37,45 @@ char **generate_seed(int rows, int cols) {
   return seed;
 }
 
+int count_live_neighbors(char **grid, int row, int col) {
+  int live_neighbors = 8;
+
+  // top
+  if ((row > 0 && col > 0 ? grid[row - 1][col - 1] : ' ') == ' ')
+    live_neighbors--;
+
+  if ((row > 0 ? grid[row - 1][col] : ' ') == ' ')
+    live_neighbors--;
+
+  if ((row > 0 && col < (COLS - 1) ? grid[row - 1][col + 1] : ' ') == ' ')
+    live_neighbors--;
+
+  // middle
+  if ((col > 0 ? grid[row][col - 1] : ' ') == ' ')
+    live_neighbors--;
+
+  if ((col < (COLS - 1) ? grid[row][col + 1] : ' ') == ' ')
+    live_neighbors--;
+
+  // bottom
+  if ((row < (ROWS - 1) && col > 0 ? grid[row + 1][col - 1] : ' ') == ' ')
+    live_neighbors--;
+
+  if ((row < (ROWS - 1) ? grid[row + 1][col] : ' ') == ' ')
+    live_neighbors--;
+
+  if ((row < (ROWS - 1) && col < (COLS - 1) ? grid[row + 1][col + 1] : ' ') ==
+      ' ')
+    live_neighbors--;
+
+  return live_neighbors;
+}
+
 void evaluate(char **grid) {
   for (int row = 0; row < ROWS; row++) {
     for (int col = 0; col < COLS; col++) {
-      int live_neighbors = 8;
-
-      // top
-      char topl = row > 0 && col > 0 ? grid[row - 1][col - 1] : ' ';
-      if (topl == ' ')
-        live_neighbors--;
-
-      char top = row > 0 ? grid[row - 1][col] : ' ';
-      if (top == ' ')
-        live_neighbors--;
-
-      char topr = row > 0 && col < (COLS - 1) ? grid[row - 1][col + 1] : ' ';
-      if (topr == ' ')
-        live_neighbors--;
-
-      // middle
-      char left = col > 0 ? grid[row][col - 1] : ' ';
-      if (left == ' ')
-        live_neighbors--;
-
-      char right = col < (COLS - 1) ? grid[row][col + 1] : ' ';
-      if (right == ' ')
-        live_neighbors--;
-
-      // bottom
-      char botl = row < (ROWS - 1) && col > 0 ? grid[row + 1][col - 1] : ' ';
-      if (botl == ' ')
-        live_neighbors--;
-
-      char bot = row < (ROWS - 1) ? grid[row + 1][col] : ' ';
-      if (bot == ' ')
-        live_neighbors--;
-
-      char botr =
-          row < (ROWS - 1) && col < (COLS - 1) ? grid[row + 1][col + 1] : ' ';
-      if (botr == ' ')
-        live_neighbors--;
-
+      int live_neighbors = count_live_neighbors(grid, row, col);
+      
       // handle dead cells
       if (grid[row][col] == ' ') {
         if (live_neighbors == 3) {
